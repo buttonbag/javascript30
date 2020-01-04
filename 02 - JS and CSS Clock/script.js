f74107ac532f99c6bd82858b4206cfb23dc14f80
Louie Camacho <louiecamacho@Elizabeths-MacBook-Air.local>
2020-01-04 13:58:20 -0600

    added motion to hours and minutes hands

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,11 +1,19 @@
 const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const min = now.getMinutes();
+    const minsDegrees = ((min / 60) * 360) + 90;
+    const hour = now.getHours();
+    const hoursDegrees = ((hour / 12) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    console.log(seconds);
+    minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    // console.log(seconds);
 }
 
 setInterval(setDate, 1000);
