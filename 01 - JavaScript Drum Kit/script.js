a0a1d79790bdce5d1a2306dee40d9506a9f1480a
Louie Camacho <louiecamacho@Elizabeths-MacBook-Air.local>
2020-01-04 11:07:47 -0600

    add playing to classList to show keystroke

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,3 +1,15 @@
 window.addEventListener('keydown', function(e) {
-  console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // if there's no audio, stop function
+    audio.currentTime = 0; // start audio from beginning on keydown
+    audio.play();
+    key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    console.log(e);
+}
+
+const keys = document.querySelectorAll('key');
+keys.forEach(key => key.addEventListener('transitionEnd', removeTransition));
